edf0e7cb7f73038e383136c0a0379c5181437ca5
Kudryashov <hekpomamohtehok@gmail.com>
2014-06-03 15:31:30 +0300

    part3 v1.0

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ActiveRecord::Base
-  validates :title, presence: true
-  validates :rating, presence: true
+  validates :title, :rating, :release_date, presence: true
+  validates :title, uniqueness: { scope: :release_date, message: "The film with the same name and release date, there is restriction" }
+  validates :rating,  format: {with: /\A(G|PG|PG-13|R|NC-17)\z/, message: "Rating must be G, PG, PG-13, R or NC-17"}
+  validates :release_date, format: {with: /\A(\d{4}-\d{2}-\d{2})\z/, message: "Release date format must be yyyy-mm-dd"}
 end
