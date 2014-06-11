60fab947d934aebc99f401a200bcbe8ccc0ca8b7
KudryashovAV <hekpomamohtehok@gmail.com>
2014-06-12 01:20:11 +0400

    made tests on .list and validates

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -7,24 +7,24 @@ describe Movie do
     end
  
     it 'One movie - one rating' do
-      Movie.create(title: 'aaa', rating: 'R', release_date: Date.today)
+      Movie.create(title: 'aaa', rating: 'R', release_date: Date.today, description: 'this is a very interesting movie')
       expect(Movie.all_ratings).to eq(['R'])
     end
  
     it 'Another movie - another rating' do
-      Movie.create(title: 'aaa', rating: 'PG', release_date: Date.today)
+      Movie.create(title: 'aaa', rating: 'PG', release_date: Date.today, description: 'this is a very interesting movie')
       expect(Movie.all_ratings).to eq(['PG'])
     end
  
     it 'Two movies - two ratings' do
-      Movie.create(title: 'aaa', rating: 'PG', release_date: Date.today)
-      Movie.create(title: 'aaa', rating: 'R', release_date: Date.today)
+      Movie.create(title: 'aaa', rating: 'PG', release_date: Date.today, description: 'this is a very interesting movie')
+      Movie.create(title: 'aaa', rating: 'R', release_date: Date.today, description: 'this is a very interesting movie')
       expect(Movie.all_ratings).to eq(['PG', 'R'])
     end
  
     it 'No duplicates' do
-      Movie.create(title: 'aaa', rating: 'PG', release_date: Date.today)
-      Movie.create(title: 'aaa', rating: 'PG', release_date: Date.today)
+      Movie.create(title: 'aaa', rating: 'PG', release_date: Date.today, description: 'this is a very interesting movie')
+      Movie.create(title: 'aaa', rating: 'PG', release_date: Date.today, description: 'this is a very interesting movie')
       expect(Movie.all_ratings).to eq(['PG'])
     end
     
