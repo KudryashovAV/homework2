378c28c030da5672b8dde2eedeca61c005f6687d
Kudryashov <hekpomamohtehok@gmail.com>
2014-06-12 10:12:13 +0300

    update test(add movies from validate)

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -67,7 +67,10 @@ describe Movie do
   context "validates" do
     let(:movie1) {Movie.create(title: 'T', rating: 'PG', release_date: '2014-06-12', description: 'this is a very interesting movie')}
     let(:movie2) {Movie.create(title: 'A', rating: 'G', release_date: '1936-05-05', description: 'this is')}
-    let(:movie3) {Movie.create(title: '', rating: '', release_date: '', description: '')}
+    let(:movie3) {Movie.create(title: '', rating: 'G', release_date: '1936-05-05', description: 'this is a very interesting movie')}
+    let(:movie4) {Movie.create(title: 'B', rating: '', release_date: '1936-05-05', description: 'this is a very interesting movie')}
+    let(:movie5) {Movie.create(title: 'C', rating: 'G', release_date: '', description: 'this is a very interesting movie')}
+    let(:movie6) {Movie.create(title: 'D', rating: 'G', release_date: '1936-05-05', description: '')}
     
     
     it 'should require a title' do
@@ -77,17 +80,17 @@ describe Movie do
 
     it 'should require a release_date' do
       expect(movie1).to be_valid
-      expect(movie3).not_to be_valid
+      expect(movie5).not_to be_valid
     end
 
     it 'should require a rating' do
       expect(movie1).to be_valid
-      expect(movie3).not_to be_valid
+      expect(movie4).not_to be_valid
     end
 
     it 'Availability and description length' do
       expect(movie1).to be_valid
-      expect(movie3).not_to be_valid
+      expect(movie6).not_to be_valid
       expect(movie2).not_to be_valid
     end
    
